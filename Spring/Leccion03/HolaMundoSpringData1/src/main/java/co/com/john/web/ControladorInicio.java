060fe62f7900de0d32f7809c49626c4f62dad6da
John Casallas <jocasallas5@poligran.edu.co>
2024-05-28 22:45:26 -0500

    Nivelación 280524
    Nivelación de archivos

--- Spring/Leccion03/HolaMundoSpringData1/src/main/java/co/com/john/web/ControladorInicio.java
+++ Spring/Leccion03/HolaMundoSpringData1/src/main/java/co/com/john/web/ControladorInicio.java
@@ -1,8 +1,10 @@
-package co.com.john;
+package co.com.john.web;
 
+import co.com.john.dao.PersonaDao;
 import java.util.ArrayList;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,40 +15,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-    
-    
-    @Value("${index.saludo}")
-    private String saludo;
-    
+
+    @Autowired
+    private PersonaDao personaDao;
+
     @GetMapping("/")
-    public String inicio(Model model){
-        var mensaje = "Hola Mundo con Thymeleaf";
-        
-        var persona = new Persona();
-        persona.setNombre("John");
-        persona.setApellido("Casallas");
-        persona.setEmail("prueba@email.com");
-        persona.setTelefono("55443322");
-        
-        var persona2 = new Persona();
-        persona2.setNombre("Juan");
-        persona2.setApellido("Casallas");
-        persona2.setEmail("juan@email.com");
-        persona2.setTelefono("33229900");
-        
-//        var personas = new ArrayList();
-//        personas.add(persona);
-//        personas.add(persona2);
-
-//        var personas = Arrays.asList(persona, persona2);
-        var personas = new ArrayList();
-
-        log.info("Ejecutando el controlador Spring MVC");        
-        model.addAttribute("mensaje", mensaje);
-        model.addAttribute("saludo", saludo);
+    public String inicio(Model model) {
+        var personas = personaDao.findAll();
+//        log.info("ejecutando el controlador Spring MVC");
         model.addAttribute("personas", personas);
         return "index";
     }
-    
 }
-
